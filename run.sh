37356af0a707649c578a35c194964a36b9929919
Jose Schmucke <jose.schmucke@ab4cus.com>
2018-08-03 10:31:50 -0400

    cambios de scripts

--- run.sh
+++ run.sh
@@ -14,6 +14,18 @@ docker run --name=middleware-opendj  \
 	-v /opt/opendj/data:/opt/opendj/data  \
 	-d e4cash/opendj-3.0.0:latest
 
+# Testing
+#docker run --name=middleware-opendj  \
+#	--hostname=opendj.e4cash.net  \
+#	--network-alias=ldap \
+#	-m 1.5GB  \
+#	-p 389:389  \
+#	-p 636:636  \
+#	-p 4444:4444  \
+#	-p 8989:8989 \
+#	-v /opt/opendj/data:/opt/opendj/data  \
+#	-d e4cash/opendj-3.0.0:latest
+
 sleep 5
 
 docker ps -a
